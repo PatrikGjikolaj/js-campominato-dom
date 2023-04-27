a3478ed82ae75ef4a6f0a7000a9c7b6846be7df3
PatrikGjikolaj <123626448+PatrikGjikolaj@users.noreply.github.com>
2023-04-27 15:11:27 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//recupero la griglia contenitore
+const gioca = document.getElementById("gioca")
+
+gioca.addEventListener("click", creaBox )
+
+function creaBox(creaGriglia){
+const griglia = document.getElementById("grid");
+griglia.innerHTML= ""
+console.log(griglia)
+
+for (let i = 0; i < 100; i++) {
+    
+    const nuovoDiv = document.createElement("div");
+    nuovoDiv.classList.add("square", "d-flex","justify-content-center", "align-items-center" );
+    nuovoDiv.innerText= i+1
+
+    nuovoDiv.addEventListener("click", function() {
+        this.classList.toggle("clicked");
+        console.log(i+1)
+    });
+
+    griglia.appendChild(nuovoDiv);
+
+}
+}
