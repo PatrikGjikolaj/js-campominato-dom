34d91392b19b1f02da8e1bb4b6a24e0393ffa165
PatrikGjikolaj <123626448+PatrikGjikolaj@users.noreply.github.com>
2023-04-27 18:19:22 +0200

    esercizio completo senza bonus

--- js/main.js
+++ js/main.js
@@ -8,25 +8,28 @@ function creaBox(creaGriglia){
 const griglia = document.getElementById("grid");
 griglia.innerHTML= ""
 console.log(griglia)
-
+let contatore = 0;
 for (let i = 0; i < 100; i++) {
+    
     const nuovoDiv = document.createElement("div");
     nuovoDiv.classList.add("square", "d-flex","justify-content-center", "align-items-center" );
     nuovoDiv.innerText= i+1
 
-    nuovoDiv.addEventListener("click", function() {
-        this.classList.toggle("clicked");
-        console.log(i+1)
 
+    nuovoDiv.addEventListener("click", function() {
+        this.classList.add("clicked");
+        console.log(i+1)
+        contatore++;
 
         for (let c = 0; c < 16; c++) {
             if (nuovoDiv.innerText == numCasuali[c]) {
                 this.classList.remove("clicked");
-                this.classList.toggle("bomb");
-                document.getElementById("tentativi").innerHTML = "Hai eseguito n tentativi";
+                this.classList.add("bomb");
+                document.getElementById("tentativi").innerHTML = "Hai eseguito " + contatore + " tentativi";
             }
             
         }
+        
     });
     griglia.appendChild(nuovoDiv);
 }
